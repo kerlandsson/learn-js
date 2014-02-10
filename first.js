8b08dae02511a0e56206667ae870cf85849ca252
Kristoffer Erlandsson <erlandsson@gmail.com>
2014-02-10 18:50:58 +0100

    .

--- first.js
+++ first.js
@@ -1,11 +1,26 @@
 
-function main() {
-	console.log("Initialized!");
-	var canvas = document.getElementById("gameCanvas");
-	var ctx = canvas.getContext("2d");
-	ctx.fillRect(0,0,200,100);
+function Game(ctx) {
+	this.ctx = ctx;
 
+	this.tick = function() {
+		this.ctx.fillRect(0,0,200,100);
+	}
+
+
+}
+function setupCanvasContext() {
+	var canvas = document.createElement("canvas");
+	canvas.width = 512;
+	canvas.height = 300;
+	canvas.style.border = "1px solid";
+	document.body.appendChild(canvas);
+	return canvas.getContext("2d");
+}
+
+var game = new Game(setupCanvasContext());
+var mainLoop = function() {
+	game.tick();
 }
 
 
-window.onload = main
+setInterval(mainLoop, 100);
