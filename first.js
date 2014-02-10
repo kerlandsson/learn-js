c1e899c9d9690cc3e3062c05d10365f9dbd4994a
Kristoffer Erlandsson <erlandsson@gmail.com>
2014-02-10 20:59:42 +0100

    paddle created

--- first.js
+++ first.js
@@ -1,13 +1,49 @@
 
 function Game(ctx) {
 	this.ctx = ctx;
+	this.field = new Field(512, 300);
+	this.paddle = new Paddle(new Point(0, 285));
 
-	this.tick = function() {
-		this.ctx.fillRect(0,0,200,100);
+	this.tick = function(delta) {
+		this.field.draw(this.ctx);
+		this.paddle.draw(this.ctx);
+	}
+}
+
+
+function Paddle(startPos) {
+	var PADDLE_HEIGHT = 15;
+	var PADDLE_WIDTH = 40;
+	var PADDLE_SPEED = 100;
+	this.pos = startPos;
+
+	this.move = function (newPos) {
+		this.pos = newPos;
 	}
 
+	this.draw = function(ctx) {
+		ctx.fillRect(this.pos.x, this.pos.y, PADDLE_WIDTH, PADDLE_HEIGHT);
+	}
 
 }
+
+function Point(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+function Field(width, height) {
+	this.width = width;
+	this.height = height;
+
+	this.draw = function(ctx) {
+		ctx.save();
+		ctx.fillStyle="#F984EF";
+		ctx.fillRect(0, 0, this.width, this.height);
+		ctx.restore();
+	}
+}
+
 function setupCanvasContext() {
 	var canvas = document.createElement("canvas");
 	canvas.width = 512;
@@ -18,9 +54,17 @@ function setupCanvasContext() {
 }
 
 var game = new Game(setupCanvasContext());
-var mainLoop = function() {
-	game.tick();
+
+var lastTickAt = null;
+function tick(timestamp) {
+	if (!this.lastTickAt) {
+		this.lastTickAt = timestamp;
+	}
+	var delta = timestamp - lastTickAt;
+	lastTickAt = timestamp;
+	game.tick(delta);
+	requestAnimationFrame(tick)
 }
 
+requestAnimationFrame(tick);
 
-setInterval(mainLoop, 100);
