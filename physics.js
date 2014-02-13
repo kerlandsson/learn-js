2f9c438cb41ff11411107b507c6fea25c4f39907
Kristoffer Erlandsson <erlandsson@gmail.com>
2014-02-13 14:17:32 +0100

    intersection tests

--- physics.js
+++ physics.js
@@ -7,8 +7,8 @@ function Rectangle(x, y, w, h) {
 }
 
 function intersects(r1, r2) {
-        return r1.x < r2.x + r2.w
-                        && r1.x + r1.w > r2.x
-                        && r1.y < r2.y + r2.h
-                        && r1.y + r1.h;
+        return r1.x <= r2.x + r2.w
+                        && r1.x + r1.w >= r2.x
+                        && r1.y <= r2.y + r2.h
+                        && r1.y + r1.h >= r2.y;
 }
--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,34 @@
+var r = new Rectangle(10,20,30,40); // test rectangle
 test( "create rect", function() {
-	var r = new Rectangle(1,2,10,20);
-	equal(r.x, 1);
-	equal(r.y, 2);
-	equal(r.w, 10);
-	equal(r.h, 20);
+	equal(r.x, 10);
+	equal(r.y, 20);
+	equal(r.w, 30);
+	equal(r.h, 40);
 });
 
-test ( "intersect1", function() {
-	var r1 = new Rectangle(1,2,10,20);
-	var r2 = new Rectangle(11,21,10,20);
-	
+test ( "not intersects", function() {
+	var r2 = new Rectangle(41,61,10,20);
+	ok(!intersects(r, r2));
 });
+
+test ( "intersects bottom right", function() {
+	var r2 = new Rectangle(40, 60, 10, 10);
+	ok(intersects(r, r2));
+});
+
+test ( "intersects top right", function() {
+	var r2 = new Rectangle(10, 0, 10, 20);
+	ok(intersects(r, r2));
+});
+
+test ( "intersects top left", function() {
+	var r2 = new Rectangle(0, 0, 10, 20);
+	ok(intersects(r, r2));
+});
+
+test ( "intersects bottom left", function() {
+	var r2 = new Rectangle(0, 20, 10, 10);
+	ok(intersects(r, r2));
+});
+
+
