56f2e676b26b536085a15519a936c3f57e9429b6
Kristoffer Erlandsson <erlandsson@gmail.com>
2014-02-19 21:30:19 +0100

    messing around with b2box more

--- b2-physics.js
+++ b2-physics.js
@@ -1,7 +1,14 @@
 var b2Vec2 = Box2D.Common.Math.b2Vec2, b2AABB = Box2D.Collision.b2AABB, b2BodyDef = Box2D.Dynamics.b2BodyDef, b2Body = Box2D.Dynamics.b2Body, b2FixtureDef = Box2D.Dynamics.b2FixtureDef, b2Fixture = Box2D.Dynamics.b2Fixture, b2World = Box2D.Dynamics.b2World, b2MassData = Box2D.Collision.Shapes.b2MassData, b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape, b2CircleShape = Box2D.Collision.Shapes.b2CircleShape, b2DebugDraw = Box2D.Dynamics.b2DebugDraw, b2Shape = Box2D.Collision.Shapes.b2Shape, b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef;
 
-var BODY_TYPE = {STATIC : 0, DYNAMIC: 1};
-var DEFAULTS = {DENSITY : 1.0, FRICTION : 0.5, RESTITUTION : 0.4};
+var BODY_TYPE = {
+	STATIC : 0,
+	DYNAMIC : 1
+};
+var DEFAULTS = {
+	DENSITY : 1.0,
+	FRICTION : 0.5,
+	RESTITUTION : 0.4
+};
 
 function PhysicsWorld(gravityX, gravityY) {
 	this.world = new b2World(new b2Vec2(gravityX, gravityY), true);
@@ -15,8 +22,15 @@ PhysicsWorld.prototype.step = function(delta) {
 
 PhysicsWorld.prototype.updateBodyStates = function() {
 	for (var b = this.world.GetBodyList(); b != null; b = b.GetNext()) {
-		if (typeof b.GetUserData() !== 'undefined' && b.GetUserData() != null)  {
-			objState = {x: b.GetPosition().x, y: b.GetPosition().y, angle : b.GetAngle()};
+		if (typeof b.GetUserData() !== 'undefined' && b.GetUserData() != null) {
+			var velocity = b.GetLinearVelocity();
+			objState = {
+				x : b.GetPosition().x,
+				y : b.GetPosition().y,
+				angle : b.GetAngle(),
+				vx : velocity.x,
+				vy : velocity.y
+			};
 			ourObj = b.GetUserData();
 			ourObj.update(objState);
 		}
@@ -24,10 +38,11 @@ PhysicsWorld.prototype.updateBodyStates = function() {
 };
 
 PhysicsWorld.prototype.addBody = function(body, bodyProperties) {
+	// TODO don't return the body, facade it somehow
 	if (body instanceof RectangleEntity) {
-		this.addRectangleBody(body, bodyProperties);
+		return this.addRectangleBody(body, bodyProperties);
 	} else if (body instanceof CircleEntity) {
-		this.addCircleBody(body, bodyProperties);
+		return this.addCircleBody(body, bodyProperties);
 	}
 };
 
@@ -35,33 +50,47 @@ PhysicsWorld.prototype.addRectangleBody = function(rect, bodyProperties) {
 	var fixDef = new b2FixtureDef();
 	fixDef.shape = new b2PolygonShape;
 	fixDef.shape.SetAsBox(rect.halfWidth, rect.halfHeight);
-	this.addBodyWithFixdef(rect, bodyProperties, fixDef);
+	return this.addBodyWithFixdef(rect, bodyProperties, fixDef);
 };
 
 PhysicsWorld.prototype.addCircleBody = function(circle, bodyProperties) {
 	var fixDef = new b2FixtureDef();
 	fixDef.shape = new b2CircleShape(circle.radius);
-	this.addBodyWithFixdef(circle, bodyProperties, fixDef);
+	return this.addBodyWithFixdef(circle, bodyProperties, fixDef);
 };
 
-PhysicsWorld.prototype.addBodyWithFixdef = function(body, bodyProperties, fixDef) {
+PhysicsWorld.prototype.addBodyWithFixdef = function(body, bodyProperties,
+		fixDef) {
 	var bodyDef = new b2BodyDef();
-	bodyDef.type = bodyProperties.type === BODY_TYPE.DYNAMIC ? b2Body.b2_dynamicBody : b2Body.b2_staticBody;
+	setBodyDefProperties(bodyDef, bodyProperties);
+	setFixDefProperties(fixDef, bodyProperties);
+	bodyDef.position.Set(body.x, body.y);
+	bodyDef.angle = body.angle;
+	bodyDef.userData = body;
+	var createdBody=this.world.CreateBody(bodyDef);
+	createdBody.CreateFixture(fixDef);
+	return createdBody;
+};
+
+function setBodyDefProperties(bodyDef, bodyProperties) {
+	bodyDef.type = bodyProperties.type === BODY_TYPE.DYNAMIC ? b2Body.b2_dynamicBody
+			: b2Body.b2_staticBody;
 	if (bodyProperties.linearDamping != undefined) {
 		bodyDef.linearDamping = bodyProperties.linearDamping;
 	}
 	if (bodyProperties.angularDamping != undefined) {
 		bodyDef.angularDamping = bodyProperties.angularDamping;
 	}
-	setFixDefProperties(fixDef, bodyProperties);
-	bodyDef.position.Set(body.x, body.y);
-	bodyDef.angle = body.angle;
-	bodyDef.userData = body;
-	this.world.CreateBody(bodyDef).CreateFixture(fixDef);	
-};
+	if (bodyProperties.fixedRotation !== undefined) {
+		bodyDef.fixedRotation = bodyProperties.fixedRotation;
+	}
+}
 
 function setFixDefProperties(fixDef, bodyProperties) {
-	fixDef.friction = bodyProperties.friction !== undefined ? bodyProperties.friction : DEFAULTS.FRICTION;
-	fixDef.density = bodyProperties.density !== undefined ? bodyProperties.density : DEFAULTS.DENSITY;
-	fixDef.restitution = bodyProperties.restitution !== undefined ? bodyProperties.restitution : DEFAULTS.RESTITUTION;
+	fixDef.friction = bodyProperties.friction !== undefined ? bodyProperties.friction
+			: DEFAULTS.FRICTION;
+	fixDef.density = bodyProperties.density !== undefined ? bodyProperties.density
+			: DEFAULTS.DENSITY;
+	fixDef.restitution = bodyProperties.restitution !== undefined ? bodyProperties.restitution
+			: DEFAULTS.RESTITUTION;
 }
--- learn-box2d.js
+++ learn-box2d.js
@@ -1,21 +1,39 @@
+var CONSTANTS = {KEY_LEFT: 37,
+		KEY_UP: 38,
+		KEY_RIGHT: 39};
+
 var CONFIG = {
-	GAME_WIDTH : 600,
-	GAME_HEIGHT : 300,
+	GAME_WIDTH : 20, // meters
+	GAME_HEIGHT : 10, // meters
 	BGCOLOR : "#00bfff",
 	BALL_RADIUS : 5,
-	DRAW_SCALE : 30
+	DRAW_SCALE : 30 // pixels per "meter"
 };
+
+var keysDown = {};
+addEventListener("keydown", function(e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function(e) {
+	delete keysDown[e.keyCode];
+}, false);
+
 
 function Entity(x, y, angle) {
 	this.x = x;
 	this.y = y;
 	this.angle = (angle === undefined ? 0 : angle);
+	this.vx = 0;
+	this.vy = 0;
 }
 
 Entity.prototype.update = function(state) {
 	this.x = state.x;
 	this.y = state.y;
 	this.angle = state.angle;
+	this.vx = state.vx;
+	this.vy = state.vy;
 };
 
 function RectangleEntity(x, y, halfWidth, halfHeight, angle) {
@@ -34,23 +52,45 @@ function CircleEntity(x, y, radius, angle) {
 CircleEntity.prototype = new Entity();
 CircleEntity.prototype.constructor = CircleEntity;
 
+
 function Game() {
 	this.pw = new PhysicsWorld(0, 10);
 	var myRect = new RectangleEntity(3, 3, 2, 2, Math.PI / 9);
-	var ground = new RectangleEntity(0, 9.8, 20, 0.2);
+	var ground = new RectangleEntity(CONFIG.GAME_WIDTH / 2, CONFIG.GAME_HEIGHT - 0.2, CONFIG.GAME_WIDTH / 2, 0.2);
 	var obstacle = new RectangleEntity(2, 9.5, 0.3, 1);
 	var ball = new CircleEntity(8, 3, 0.4);
+	this.char = new RectangleEntity(5, 9, 0.4, 1);
 
 	this.pw.addBody(myRect, {type: BODY_TYPE.DYNAMIC});
-	this.pw.addBody(ground, {type: BODY_TYPE.STATIC});
+	this.pw.addBody(ground, {type: BODY_TYPE.STATIC, restitution: 0, friction: 0.2});
 	this.pw.addBody(obstacle, {type: BODY_TYPE.DYNAMIC});
-	this.pw.addBody(ball, {type: BODY_TYPE.DYNAMIC, angularDamping:2});
+	this.pw.addBody(ball, {type: BODY_TYPE.DYNAMIC, angularDamping: 2});
+	this.charBody = this.pw.addBody(this.char, {type: BODY_TYPE.DYNAMIC, fixedRotation: true, friction: 50, restitution: 0});
 
-	this.bodies = [myRect, ground, obstacle, ball];
+	this.bodies = [myRect, ground, obstacle, ball, this.char];
 }
 
 Game.prototype.tick = function(delta) {
 	this.pw.step(delta);
+	this.handleInput();
+};
+
+Game.prototype.handleInput = function() {
+	var desiredVelocity = 0;
+	if (CONSTANTS.KEY_LEFT in keysDown) {
+		desiredVelocity -= 10;
+	}
+	if (CONSTANTS.KEY_RIGHT in keysDown) {
+		desiredVelocity += 10;
+	}
+	if (CONSTANTS.KEY_UP in keysDown) {
+		this.charBody.ApplyImpulse(new b2Vec2(0, -1), this.charBody.GetWorldCenter());
+	}
+	if (desiredVelocity != 0) {
+		var velChange = desiredVelocity - this.char.vx;
+		var impulse = this.charBody.GetMass() * velChange;
+		this.charBody.ApplyImpulse(new b2Vec2(impulse, 0), this.charBody.GetWorldCenter());
+	}
 };
 
 function GameRenderer(ctx, game, drawScale) {
@@ -60,7 +100,7 @@ function GameRenderer(ctx, game, drawScale) {
 }
 
 GameRenderer.prototype.render = function() {
-	this.ctx.clearRect(0, 0, CONFIG.GAME_WIDTH, CONFIG.GAME_HEIGHT);
+	this.ctx.clearRect(0, 0, CONFIG.DRAW_SCALE * CONFIG.GAME_WIDTH, CONFIG.DRAW_SCALE * CONFIG.GAME_HEIGHT);
 	//world.DrawDebugData();
 	var bodies = this.game.bodies;
 	for (var i = 0; i < bodies.length; i++) {
@@ -100,7 +140,7 @@ GameRenderer.prototype.drawCircle = function(circle) {
 	ctx.restore();
 };
 
-var ctx = setupCanvasContext(CONFIG.GAME_WIDTH, CONFIG.GAME_HEIGHT);
+var ctx = setupCanvasContext(CONFIG.DRAW_SCALE * CONFIG.GAME_WIDTH, CONFIG.DRAW_SCALE * CONFIG.GAME_HEIGHT);
 
 function enableDebugDraw() {
 	var debugDraw = new b2DebugDraw();
