536fcf39f6ddd8d3d6a37c0541fe61526b0cc58e
Kristoffer Erlandsson <erlandsson@gmail.com>
2014-02-19 09:37:16 +0100

    refactoring physics to a separate file

---
+++ b2-physics.js
@@ -0,0 +1,50 @@
+var b2Vec2 = Box2D.Common.Math.b2Vec2, b2AABB = Box2D.Collision.b2AABB, b2BodyDef = Box2D.Dynamics.b2BodyDef, b2Body = Box2D.Dynamics.b2Body, b2FixtureDef = Box2D.Dynamics.b2FixtureDef, b2Fixture = Box2D.Dynamics.b2Fixture, b2World = Box2D.Dynamics.b2World, b2MassData = Box2D.Collision.Shapes.b2MassData, b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape, b2CircleShape = Box2D.Collision.Shapes.b2CircleShape, b2DebugDraw = Box2D.Dynamics.b2DebugDraw, b2Shape = Box2D.Collision.Shapes.b2Shape, b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef;
+
+var BODY_TYPE = {STATIC : 0, DYNAMIC: 1};
+var DEFAULTS = {DENSITY : 1.0, FRICTION : 0.5, RESTITUTION : 0.6};
+
+var fixDef = new b2FixtureDef();
+var bodyDef = new b2BodyDef();
+
+function PhysicsWorld(gravityX, gravityY) {
+	this.world = new b2World(new b2Vec2(gravityX, gravityY), true);
+}
+
+PhysicsWorld.prototype.step = function(delta) {
+	this.world.Step(delta / 1000, 10, 10);
+	this.world.ClearForces();
+	this.updateBodyStates();
+};
+
+PhysicsWorld.prototype.updateBodyStates = function() {
+	for (var b = this.world.GetBodyList(); b != null; b = b.GetNext()) {
+		if (typeof b.GetUserData() !== 'undefined' && b.GetUserData() != null)  {
+			objState = {x: b.GetPosition().x, y: b.GetPosition().y, angle : b.GetAngle()};
+			ourObj = b.GetUserData();
+			ourObj.update(objState);
+		}
+	}
+};
+
+PhysicsWorld.prototype.addBody = function(body, bodyProperties) {
+	if (body instanceof RectangleEntity) {
+		this.addRectangleBody(body, bodyProperties);
+	}
+};
+
+PhysicsWorld.prototype.addRectangleBody = function(rect, bodyProperties) {
+	bodyDef.type = bodyProperties.type === BODY_TYPE.DYNAMIC ? b2Body.b2_dynamicBody : b2Body.b2_staticBody;
+	fixDef.shape = new b2PolygonShape;
+	fixDef.shape.SetAsBox(rect.halfWidth, rect.halfHeight);
+	setFixDefProperties(fixDef, bodyProperties);
+	bodyDef.position.Set(rect.x, rect.y);
+	bodyDef.angle = rect.angle;
+	bodyDef.userData = rect;
+	this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+};
+
+function setFixDefProperties(fixDef, bodyProperties) {
+	fixDef.friction = bodyProperties.friction !== undefined ? bodyProperties.friction : DEFAULTS.FRICTION;
+	fixDef.density = bodyProperties.density !== undefined ? bodyProperties.density : DEFAULTS.DENSITY;
+	fixDef.restitution = bodyProperties.restitution !== undefined ? bodyProperties.restitution : DEFAULTS.RESTITUTION;
+}
--- learn-box2d.js
+++ learn-box2d.js
@@ -8,7 +8,7 @@ var CONFIG = {
 
 var SCALE = 30;
 
-var b2Vec2 = Box2D.Common.Math.b2Vec2, b2AABB = Box2D.Collision.b2AABB, b2BodyDef = Box2D.Dynamics.b2BodyDef, b2Body = Box2D.Dynamics.b2Body, b2FixtureDef = Box2D.Dynamics.b2FixtureDef, b2Fixture = Box2D.Dynamics.b2Fixture, b2World = Box2D.Dynamics.b2World, b2MassData = Box2D.Collision.Shapes.b2MassData, b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape, b2CircleShape = Box2D.Collision.Shapes.b2CircleShape, b2DebugDraw = Box2D.Dynamics.b2DebugDraw, b2Shape = Box2D.Collision.Shapes.b2Shape, b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef;
+
 
 function Entity(x, y, angle) {
 	this.x = x;
@@ -22,8 +22,8 @@ Entity.prototype.update = function(state) {
 	this.angle = state.angle;
 };
 
-function RectangleEntity(x, y, halfWidth, halfHeight) {
-	Entity.call(this, x, y);
+function RectangleEntity(x, y, halfWidth, halfHeight, angle) {
+	Entity.call(this, x, y, angle);
 	this.halfWidth = halfWidth;
 	this.halfHeight = halfHeight;
 }
@@ -52,19 +52,17 @@ Game.prototype.tick = function(delta) {
 
 };
 
-function GameRenderer(ctx, world) {
+function GameRenderer(ctx) {
 	this.ctx = ctx;
-	this.world = world;
 }
+
+var objects = [];
 
 GameRenderer.prototype.render = function() {
 	this.ctx.clearRect(0, 0, CONFIG.GAME_WIDTH, CONFIG.GAME_HEIGHT);
 	//world.DrawDebugData();
-	for (var b = world.GetBodyList(); b != null; b = b.GetNext()) {
-		if (typeof b.GetUserData() !== 'undefined' && b.GetUserData() != null)  {
-			ourObj = b.GetUserData();
-			ourObj.draw(this.ctx);
-		}
+	for (var i = 0; i < objects.length; i++) {
+		objects[i].draw(this.ctx);
 	}
 };
 
@@ -80,7 +78,7 @@ function enableDebugDraw() {
 	world.SetDebugDraw(debugDraw);
 }
 
-var renderer = new GameRenderer(ctx, world);
+var renderer = new GameRenderer(ctx);
 var game = new Game();
 
 function renderAndRequestNewFrame() {
@@ -88,47 +86,26 @@ function renderAndRequestNewFrame() {
 	requestAnimationFrame(t.tick);
 }
 
-var world = new b2World(new b2Vec2(0, 10) // gravity
-, true // allow sleep
-);
+
+var pw = new PhysicsWorld(0, 10);
 
 function tickGame(delta) {
-	world.Step(delta / 1000, 10, 10);
-	world.ClearForces();
+	pw.step(delta);
 	game.tick(delta);
-	for (var b = world.GetBodyList(); b != null; b = b.GetNext()) {
-		if (typeof b.GetUserData() !== 'undefined' && b.GetUserData() != null)  {
-			objState = {x: b.GetPosition().x, y: b.GetPosition().y, angle : b.GetAngle()};
-			ourObj = b.GetUserData();
-			ourObj.update(objState);
-		}
-	}
 }
 
-
-var fixDef = new b2FixtureDef;
-fixDef.density = 1.0;
-fixDef.friction = 0.5;
-fixDef.restitution = 0.6;
-
-var bodyDef = new b2BodyDef;
-
-function createB2Rect(rect, userData, type) {
-	bodyDef.type = type;
-	fixDef.shape = new b2PolygonShape;
-	fixDef.shape.SetAsBox(rect.halfWidth, rect.halfHeight);
-	bodyDef.position.Set(rect.x, rect.y);
-	bodyDef.userData = userData;
-	world.CreateBody(bodyDef).CreateFixture(fixDef);	
-}
-
-var myRect = new RectangleEntity(3, 3, 2, 2);
+var myRect = new RectangleEntity(3, 3, 2, 2, Math.PI / 9);
 var ground = new RectangleEntity(0, 9.8, 20, 0.2);
 var obstacle = new RectangleEntity(2, 9.5, 0.3, 1);
 
-createB2Rect(myRect, myRect, b2Body.b2_dynamicBody);
-createB2Rect(ground, ground, b2Body.b2_staticBody);
-createB2Rect(obstacle, obstacle, b2Body.b2_dynamicBody);
+pw.addBody(myRect, {type: BODY_TYPE.DYNAMIC});
+pw.addBody(ground, {type: BODY_TYPE.STATIC});
+pw.addBody(obstacle, {type: BODY_TYPE.DYNAMIC});
+
+objects.push(myRect);
+objects.push(ground);
+objects.push(obstacle);
+
 
 var t = new Ticker(tickGame, renderAndRequestNewFrame);
 renderAndRequestNewFrame();
